251a4b9c1fc2852189940e9e7f3e8ec8528e7fa3
snxx-lppxx <snmgreenx@gmail.com>
2021-12-01 03:21:42 +0500

    minor

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@
 # https://github.com/snxx-lppxx/toit-sublime/blob/master/LICENSE
 ''' GitHub:                          snxx-lppxx/toit-sublime '''
 
+from threading import Lock
+
 from .script import *
 
 import os, sys, json
@@ -17,4 +19,34 @@ import sublime_plugin
 if sys.version_info < (3, 3):
 	raise RuntimeError("Toit syntax works with (only) Sublime Text 3")
 
-def plugin_loaded():
+class Build(sublime_plugin.WindowCommand):
+
+	encoding = 'utf-8'
+	killed = False
+	proc = None
+	panel = None
+	panel_lock = Lock()
+
+	def is_enabled(self, lint=False, integration=False, kill=False):
+		if kill:
+			return self.proc is not None and self.proc.poll() is None
+		return True
+
+	def run(self, lint=False, integration=False, kill=False):
+		if kill:
+			if self.proc:
+				self.killed = True
+				self.proc.terminate()
+			return
+
+		var = self.window.extract_variables()
+		working_dir = var['file_path']
+
+		with self.panel_lock:
+			self.panel = self.window.create_output_panel('exec')
+
+			settings = self.panel.settings()
+			settings.set(
+				'result_line_regex',
+				r'^\sline (\d+) col (\d+)'
+			)
--- test/syntax_test.toit
+++ test/syntax_test.toit
@@ -1,5 +1,3 @@
-// SYNTAX TEST "Toit.sublime-syntax"
-
 main:
 	print "Toit Syntax Version: v1.0.0"
 	print "[23.11.2021]"
